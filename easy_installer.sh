dd372991e93fa4db0524d78f1f73eb89c535d70d
Undersc0re22 <35875026+undersc0re22@users.noreply.github.com>
2018-02-12 17:35:50 -0500

    Create easy_installer.sh

---
+++ easy_installer.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+yes "" | sudo apt-get install build-essential python-dev gcc-4.9 g++-4.9 git cmake libboost1.58-all-dev librocksdb-dev
+export CXXFLAGS="-std=gnu++11"
+git clone https://github.com/undersc0re22/ZomboCoin
+cd zombocoin
+mkdir build && cd $_
+cmake ..
+make
